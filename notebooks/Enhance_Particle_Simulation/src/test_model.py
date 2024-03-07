141e4aa00f50eb7f3057cbd0c78c46ce11fcedca
ElManzanero <oriol.manzano@epitech.eu>
2024-03-07 18:04:07 -0500

    [ADD] Enhance_particle_simulation

--- notebooks/Enhance_Particle_Simulation/src/test_model.py
+++ notebooks/Enhance_Particle_Simulation/src/test_model.py
@@ -1,23 +1,16 @@
 import numpy as np
 import torch
-from train_model import ParticleModel
-
-# Define a simple neural network model
-#class ParticleModel(torch.nn.Module):
-#    def __init__(self, num_input_params, num_output_params):
-#        super(ParticleModel, self).__init__()
-#        self.fc1 = torch.nn.Linear(num_input_params, 64)
-#        self.fc2 = torch.nn.Linear(64, 64)
-#        self.fc3 = torch.nn.Linear(64, num_output_params)
-        
-#    def forward(self, x):
-#        x = torch.relu(self.fc1(x))
-#        x = torch.relu(self.fc2(x))
-#        x = self.fc3(x)
-#        return x
+from importlib import reload
+from model import *
 
 def integrate_predicted_behavior(predicted_behavior):
     print(predicted_behavior)
+    print(len(predicted_behavior))
+    percentage = 1
+    for idx, point in enumerate(hou.pwd().geometry().points()):
+        print(predicted_behavior[idx])
+        predicted_behavior[idx] = predicted_behavior[idx]*percentage + np.array(point.position())*(1-percentage)
+        point.setPosition( predicted_behavior[idx].tolist())
 
 def enhance_particle_simulation(input_params, model_path):
     model = torch.load(model_path)  # Load the trained model
@@ -27,11 +20,9 @@ def enhance_particle_simulation(input_params, model_path):
         # Integrate predicted behavior into particle simulation
         integrate_predicted_behavior(predicted_behavior)
 
-
 # Parameters
-num_input_params = 3 
 model_path = '/home/oriol/tools/DailyTips/notebooks/Enhance_Particle_Simulation/models/particle_model.pth'
-
-# Use the trained model to enhance particle simulation
-new_input_params = np.random.rand(1, num_input_params)  # New input parameters for simulation
-enhance_particle_simulation(new_input_params, model_path)
+node = hou.pwd()
+geo = node.geometry()
+new_input_parms = [[p.position().x(),p.position().y(), p.position().z()] for p in geo.points()]
+enhance_particle_simulation(new_input_parms, model_path)
--- notebooks/Enhance_Particle_Simulation/src/train_model.py
+++ notebooks/Enhance_Particle_Simulation/src/train_model.py
@@ -4,42 +4,21 @@ import torch
 import ast
 from model import ComplexNet
 # Define a simple neural network model
-
-
 # Train the machine learning model
 def train_model(x, y):
     model = ComplexNet()
     criterion = torch.nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    
     for epoch in range(num_epochs):
         inputs = torch.tensor(x, dtype=torch.float32)
         targets = torch.tensor(y, dtype=torch.float32)
-        
         optimizer.zero_grad()
         outputs = model(inputs)
-        #print(targets.shape)
-        #print(outputs.shape)
         loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
-        
         print(f'Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}')
     return model
-
-# Integrate machine learning-driven behaviors into particle simulation
-def enhance_particle_simulation(input_params, model_path):
-    model = torch.load(model_path)  # Load the trained model
-    with torch.no_grad():
-        inputs = torch.tensor(input_params, dtype=torch.float32)
-        predicted_behavior = model(inputs).numpy()
-        # Integrate predicted behavior into particle simulation
-        integrate_predicted_behavior(predicted_behavior)
-
-# Mock function to integrate predicted behavior into particle simulation
-def integrate_predicted_behavior(predicted_behavior):
-    print("Integrating predicted behavior into particle simulation...")
-    # Placeholder function for integrating predicted behavior into particle simulation
 
 def generate_syntetic_data(num_input_params, num_output_param, num_samples):
     # Generate synthetic particle data for training
@@ -63,18 +42,14 @@ def retrieve_particles(points_data_csv):
         else:
             x = np.concatenate((x, points_position))
             y = np.concatenate((y, next_frame_points_position))
-    print(np.shape(x))
-    print(np.shape(y))#(121, 2500, 3)
     return x, y
+
 # Parameters
-num_input_params = 3  # Number of input parameters affecting particle behavior
-num_output_params = 3  # Number of output parameters representing particle behavior
-num_samples = 1000  # Number of training samples
 num_epochs = 400
 model_path = '/home/oriol/tools/DailyTips/notebooks/Enhance_Particle_Simulation/models/particle_model.pth'
 points_data_csv = '/home/oriol/tools/DailyTips/notebooks/Enhance_Particle_Simulation/data/test.csv'
 x, y = retrieve_particles(points_data_csv)
-#input_params, simulated_behavior = generate_syntetic_data(num_input_params, num_output_params, num_samples)
+#input_params, simulated_behavior = generate_syntetic_data(3, 3, 1000)
 # Train the machine learning model
 model = train_model(x, y)
 # Save the trained model
